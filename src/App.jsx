799794cd47ffb83f789a893cf38b6bde193b1f69
DSantosPascual <d.santos.pascual@gmail.com>
2025-03-03 17:44:01 +0100

    funcionando

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
-import './App.css'
+import React, { useState } from 'react';
 
-function App() {
- //Aquí tu código
-}
+const App = () => {
+  const [nombre, setNombre] = useState("Sofía");
+  const [nuevoNombre, setNuevoNombre] = useState('');
 
-export default App
+  const manejarClic = () => {
+    if (nuevoNombre.trim() !== '') {
+      setNombre(nuevoNombre);
+      setNuevoNombre('');
+    }
+  };
+
+  return (
+    <div>
+      <h2>Nombre del Profesor: {nombre}</h2>
+      <div>
+        <input
+          type="text"
+          value={nuevoNombre}
+          onChange={(e) => setNuevoNombre(e.target.value)}
+          placeholder="Ingresa un nombre"
+        />
+        <button onClick={manejarClic}>Actualizar</button>
+      </div>
+    </div>
+  );
+};
+
+export default App;
